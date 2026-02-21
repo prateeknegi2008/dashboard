5a2a9d4fa2f2a21b71f2ae5d5ea2d312b4227718
prateeknegi2008 <prateeknegi2008@yahoo.com>
2026-02-22 00:35:04 +0530

    initial commit

---
+++ dashboard.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import func_filter_dataframe as fd
+
+
+# Hide the "Deploy" button
+st.markdown(
+    """
+    <style>
+    .stAppDeployButton {
+        display:none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# (Optional) Hide the hamburger menu (upper right)
+st.markdown(
+    """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+##########
+
+
+
+st.header("RAAM Dashboard")
+st.set_page_config(layout="wide") 
+
+df = pd.read_csv("dashboard.csv")
+filtered_df = fd.filter_dataframe(df)
+st.dataframe(filtered_df, use_container_width=True)
