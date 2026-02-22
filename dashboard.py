5e8c255fa56cc4401468e73a4695be688e775ffb
prateeknegi2008 <prateeknegi2008@yahoo.com>
2026-02-22 19:41:25 +0530

    added column id and url links

--- dashboard.py
+++ dashboard.py
@@ -34,5 +34,12 @@ st.header("RAAM Dashboard")
 st.set_page_config(layout="wide") 
 
 df = pd.read_csv("dashboard.csv")
+
+df.index=pd.RangeIndex(start=1, stop=len(df) + 1, step=1)
+df.index.name = "ID"
 filtered_df = fd.filter_dataframe(df)
-st.dataframe(filtered_df, use_container_width=True)
+st.dataframe(filtered_df, use_container_width=True, column_config={
+        "URL": st.column_config.LinkColumn(
+            "URL",      # The label shown at the top of the column
+        )
+    })
